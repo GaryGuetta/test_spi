517abf0cb795fc33a03033bd1478803ce9f8184e
GaryGuetta <65896218+GaryGuetta@users.noreply.github.com>
2026-03-12 16:13:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,30 +6,19 @@ const SUPABASE_KEY = "sb_publishable_A3J91eJqawcbb2s6a-8vRQ_9bOFqaE_"
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
 async function loadUsers() {
-  const { data } = await supabase
+
+  const { data, error } = await supabase
     .from("users")
     .select("*")
 
   const list = document.getElementById("users")
-  list.innerHTML = ""
 
   data.forEach(user => {
     const li = document.createElement("li")
     li.textContent = user.name + " - " + user.email
     list.appendChild(li)
   })
-}
 
-window.addUser = async function () {
-
-  const name = document.getElementById("name").value
-  const email = document.getElementById("email").value
-
-  await supabase
-    .from("users")
-    .insert([{ name, email }])
-
-  loadUsers()
 }
 
 loadUsers()
